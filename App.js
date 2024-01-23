6bc129ec7edc00c79c931f9bc926028568e21483
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-01-23 09:39:47 -0600

    23/01/24

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>**Hola 5C**</Text>
-      <Text>**Hola 5C**</Text>
-      <Text>**Hola 5C**</Text>
-      <Text>**Hola 5C**</Text>
-      <Text>**Hola 5C**</Text>
-      <Text>**Hola 5C**</Text>
-      <TextInput placeholder='Hola chicos***'></TextInput>
+      <View style={styles.encabezado}>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>Encabezado</Text>
+        <Text style={styles.texto}>X</Text>
+      </View>
+      <View style={styles.cuerpo}>
+        <Text style={styles.texto}>Cuerpo</Text>
+      </View>
+      <View style={styles.pie}>
+        <Text style={styles.texto}>Pie</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,7 +24,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'lightblue',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+
   },
+  encabezado:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    backgroundColor:'#1373e8',
+  },
+  cuerpo:{
+    flex:8
+  },
+  pie:{
+    flex:1,
+    backgroundColor:'#1373e8',
+  },
+  texto:{
+    fontSize:25,
+    color:'#fff'
+  }
 });
