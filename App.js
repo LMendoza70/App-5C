526f887b2c3ce9e64538e434875c7260062abffb
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-01-25 08:17:22 -0600

    25/01/24

--- App.js
+++ App.js
@@ -1,23 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.encabezado}>
+      <Encabezado/>
+      <Cuerpo/>
+      <Pie/>
+      <Pie2 opA='U' opB='C' opC='E' opD='F'/>
+      <Pie3 opA={'X'} opB={'Y'} opC={'Z'} opD={'W'}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export const Encabezado=()=>{
+  return(
+    <View style={styles.encabezado}>
         <Text style={styles.texto}>X</Text>
         <Text style={styles.texto}>Encabezado</Text>
         <Text style={styles.texto}>X</Text>
       </View>
-      <View style={styles.cuerpo}>
+  )
+}
+
+export const Pie=()=>{
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+      </View>
+  )
+}
+
+export const Pie2=(props)=>{
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>{props.opA}</Text>
+        <Text style={styles.texto}>{props.opB}</Text>
+        <Text style={styles.texto}>{props.opC}</Text>
+        <Text style={styles.texto}>{props.opD}</Text>
+      </View>
+  )
+}
+
+export const Pie3=({opA,opB,opC,opD})=>{
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>{opA}</Text>
+        <Text style={styles.texto}>{opB}</Text>
+        <Text style={styles.texto}>{opC}</Text>
+        <Text style={styles.texto}>{opD}</Text>
+      </View>
+  )
+}
+
+export const Cuerpo=()=>{
+  return(
+    <View style={styles.cuerpo}>
         <Text style={styles.texto}>Cuerpo</Text>
+        <Login/>
       </View>
-      <View style={styles.pie}>
-        <Text style={styles.texto}>Pie</Text>
-      </View>
-      <StatusBar style="auto" />
+  )
+}
+
+export const Login=()=>{
+  return(
+    <View>
+      <Text>Username...</Text>
+      <TextInput></TextInput>
+      <Text>Password...</Text>
+      <TextInput></TextInput>
+      <Button title='Login' onPress={()=>Alert.alert('Logeado')} />
+      <Button title='Cancel' onPress={funcion}/>
+      <Button title='ejemplo' onPress={funcion}/>
     </View>
-  );
+  )
+}
+
+const funcion=()=>{
+  //funcion bien largota
+  Alert.alert('llamada desde la funcion')
 }
 
 const styles = StyleSheet.create({
@@ -40,7 +105,10 @@ const styles = StyleSheet.create({
   },
   pie:{
     flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-evenly',
     backgroundColor:'#1373e8',
+    alignItems:'center'
   },
   texto:{
     fontSize:25,
